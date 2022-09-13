025c28f435441e5238f31ebe89e7d079fd9b3373
fatima-zahra <58591844+fatimzehraa@users.noreply.github.com>
2022-09-14 00:32:56 +0100

    env fail

--- src/env.c
+++ src/env.c
@@ -9,40 +9,38 @@ int	clone_env(char **envp, t_vec *env)
 	(void) env;
 	return (0);
 }
-//
-//
-//
-//t_list	*clone(char **env)
-//{
-//	int		i;
-//	t_list	*lst_env;
-//
-//	lst_env = NULL;
-//	i = 0;
-//	while (env[i])
-//	{
-//		ft_lstadd_back(&lst_env, ft_lstnew(env[i]));
-//		i++;
-//	}
-//	return (lst_env);
-//}
-////TODO 
-//char	*get_env(char	*var, t_list *lst_env)
-//{
-//	t_list	*curr;
-//	char	*value;
-//
-//	value = NULL;
-//	curr = lst_env;
-//	while (curr)
-//	{
-//		if (ft_strncmp(var, curr->content, ft_strlen(var)))
-//		{
-//			value = getenv(curr->content);
-//			break ;
-//		}
-//		curr = curr->next;
-//	}
-//	return (value);
-//}
-//
+
+t_list	*clone(char **env)
+{
+	int		i;
+	t_list	*lst_env;
+
+	lst_env = NULL;
+	i = 0;
+	while (env[i])
+	{
+		ft_lstadd_back(&lst_env, ft_lstnew(env[i]));
+		i++;
+	}
+	return (lst_env);
+}
+//TODO 
+char	*get_env(char	*var, t_list *lst_env)
+{
+	t_list	*curr;
+	char	*value;
+
+	value = NULL;
+	curr = lst_env;
+	while (curr)
+	{
+		if (ft_strncmp(var, curr->content, ft_strlen(var)))
+		{
+			value = getenv(curr->content);
+			break ;
+		}
+		curr = curr->next;
+	}
+	return (value);
+}
+
