97da833cf6e8792d634126b1998be8adba9dc605
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-09-16 19:07:27 +0000

    impl: core of str lib v1

--- inc/str.h
+++ inc/str.h
@@ -1,11 +1,20 @@
 #ifndef STR_H
 #define STR_H
 
+typedef struct s_str
+{
+	char *val;
+	int size;
+	int cap;
+}	t_str;
+
 int	ft_strlen(char *s);
 int	has_chars(char c, char *chrs);
 int	is_space(char c);
 int	is_alphanum(char c);
 int	is_special(char *s);
 int	ft_strncmp(char *s1, char *s2, int n);
+void	ft_strncpy(char *dest, char *src, unsigned int size);
+char	*ft_strndup(char *src, int len);
 
 #endif
---
+++ src/str/core.c
@@ -0,0 +1,76 @@
+#include "str.h"
+#include <stdlib.h>
+
+int str_mk(t_str *str, char *s)
+{
+	str->size = ft_strlen(s);
+	str->cap = str->size;
+	str->val = ft_strndup(s, str->size);
+	if (str->val == 0)
+	{
+		str->size = -1;
+		str->cap = -1;
+	}
+	return str->val != 0;
+}
+
+int str_push(t_str *s1, t_str *s2)
+{
+	int slot;
+	char *tmp;	
+
+	slot = s1->cap - s1->size;
+	if (s2->size >= slot)
+	{
+		tmp = s1->val;
+		s1->val = malloc(sizeof(char *) * (s1->size + s2->size + 20));
+		if (s1->val == NULL)
+		{
+			s1->cap = -1;
+			s1->size = -1;
+			free(tmp);
+			return 0;
+		}
+		s1->cap = s1->size + s2->size + 20;
+		ft_strncpy(s1->val, tmp, s1->size);
+		s1->size = s1->size + s2->size;
+		free(tmp);
+	}
+	ft_strncpy(s1->val + s1->size, s2->val, s2->size);
+	return 1;
+}
+
+int str_same(t_str *s1, t_str *s2, int s2_size)
+{
+	int i;
+	if (s1->size < s2_size || s2->size < s2_size)
+		return 0;
+	i = 0;
+	while (i < s2_size) {
+		if (s1->val[i] != s2->val[i])	
+			return 0; 
+		i++;
+	}
+	return 1;
+}
+
+int str_clone(t_str *dest, t_str *src)
+{
+	char *tmp;
+
+	if (src->size < dest->size)
+	{
+		ft_strncpy(dest->val, src->val, src->size);
+		dest->size = src->size;
+		return (1);
+	}
+	tmp = ft_strndup(src->val, -1);
+	if (tmp == NULL)
+		return 0;
+	free(dest->val);
+	dest->val = tmp;
+	dest->size = src->size;
+	dest->cap = src->size;
+	return (1);
+}
+
