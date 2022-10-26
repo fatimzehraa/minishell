8d38833b155580404316af12a40900a47748f7ea
Ismail Ait bella <iait-bel@e2r2p12.1337.ma>
2022-10-26 20:59:58 +0100

    add support for $?

--- src/env.c
+++ src/env.c
@@ -3,6 +3,41 @@
 #include "list.h"
 #include "str.h"
 
+
+static int	count_size(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count++;
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(unsigned int n)
+{
+	char	*ascii_s;
+	int		i;
+
+	i = count_size(n);
+	ascii_s = malloc(i * sizeof(char) + 1);
+	if (ascii_s == NULL)
+		return (NULL);
+	ascii_s[i--] = 0;
+	while (i >= 0)
+	{
+		ascii_s[i] = n % 10 + '0';
+		n = n / 10;
+		i--;
+	}
+	return (ascii_s);
+}
+
 int	clone_env(char **envp, t_vec *env)
 {
 	int i;
@@ -25,11 +60,15 @@ t_str get_senv(t_str *str, t_vec *env)
 	char	*to_find;
 
 	str_init(&val);
-	holder = search_vec(env, str->val, str->size);
-	if (holder == -1)
-		to_find = "";
-	else
-		to_find =  env->content[holder] + str->size + 1;
+	if (str_psame(str, "?", 1))
+		to_find = ft_itoa(exit_status);
+	else {
+		holder = search_vec(env, str->val, str->size);
+		if (holder == -1)
+			to_find = "";
+		else
+			to_find =  env->content[holder] + str->size + 1;
+	}
 	str_pnclone(&val, to_find, -1); 
 	return (val);
 }
